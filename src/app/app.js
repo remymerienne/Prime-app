ba637765fda865f3bd352de4701f5d9b795b9e62
Rémy Merienne <remymerienne@hotmail.fr>
2022-03-11 16:57:07 +0100

    premier essai login full JS

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,5 @@
 import Structure from '../containers/Structure.js';
-import PrimeNumber from '../containers/PrimeNumber.js';
 import Login from '../containers/Login.js';
 
 new Structure();
 new Login();
-new PrimeNumber();
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,12 +1,25 @@
 import loginUI from '../views/LoginUI';
+import PrimeNumber from '../containers/PrimeNumber.js';
+import PASSWORD from '../constants/password';
+
 
 export default class {
 
-constructor() {
-  const main = document.querySelector('main.js-main');
-  main.innerHTML += loginUI();
-}
+  constructor() {
+    const main = document.querySelector('main.js-main');
+    main.innerHTML = loginUI();
+    this.checkUserName();
+  }
 
-
+  checkUserName() {
+    const inputUser = document.getElementById('mail');
+    inputUser.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter') {
+        if(e.target.value === PASSWORD){
+          new PrimeNumber();
+        }
+      }
+    });
+  }
 
 }
