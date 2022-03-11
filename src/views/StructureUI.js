c4af9ee6227b37123776396349c86fbae40c21b8
Rémy Merienne <remymerienne@hotmail.fr>
2022-03-11 11:28:45 +0100

    add input incons

--- src/views/StructureUI.js
+++ src/views/StructureUI.js
@@ -5,7 +5,7 @@ export default () => {
       <div class="page">
         <header class="page-header">
           <h1>Hello world</h1>
-          <span class="page-header__logo fas fa-seedling"></span>
+          <i class="page-header__logo fas fa-seedling"></i>
         </header>
         <main class="js-main">
         </main>
